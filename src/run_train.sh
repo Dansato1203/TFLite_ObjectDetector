82cd1fbac6064fb83a08ba5e1d9dad979fbf34ae
Dansato1203 <s19C1054bu@s.chibakoudai.jp>
2022-05-08 02:16:51 +0900

    fix run_train.sh

--- src/run_train.sh
+++ src/run_train.sh
@@ -6,11 +6,10 @@ python3 fix_pipeline.py
 
 python3 /src/models/research/object_detection/model_main.py \
 	--pipeline_config_path="/src/models/research/object_detection/samples/configs/ssdlite_mobiledet_edgetpu_320x320_coco_sync_4x4.config" \
-	#--model_dir="/src/train_result" \
-	--model_dir=/src/train
+	--model_dir="/src/train_logs" 
 
 python3 /content/models/research/object_detection/export_inference_graph.py \
 	--input_type=image_tensor \
 	--pipeline_config_path=/src/models/research/object_detection/samples/configs/ssdlite_mobiledet_edgetpu_320x320_coco_sync_4x4.config \
-	--output_directory=/src/inference_graph \
-	--trained_checkpoint_prefix=/content/train/model.ckpt-10000
+	--output_directory=/src/train_logs/inference_graph \
+	--trained_checkpoint_prefix=/src/train_logs/model.ckpt-10000
