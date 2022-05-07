b1a79f805f5a0d8c7ffccdcf6a3d0dd2f80ae198
Dansato1203 <s19C1054bu@s.chibakoudai.jp>
2022-05-08 02:03:47 +0900

    fix launch file

--- launch_train.sh
+++ launch_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOCKER_IMAGE="dansato1203/tf_ball_detection:latest"
+DOCKER_IMAGE="tf_ball_detection:latest"
 
 if ! command -v nvidia-smi &> /dev/null
 then
@@ -10,4 +10,4 @@ fi
 docker pull "$DOCKER_IMAGE"
 docker run --gpus=all -it --rm -e DISPLAY --privileged -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
 	--name tf_ball_detection \
-	$DOCKER_IMAGE /run_train.sh
+	$DOCKER_IMAGE
